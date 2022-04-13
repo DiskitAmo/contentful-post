2bab81a90443bc65add28ed846c2a694d8a3b7a3
diskit <diskit.angmo@catalyst.sh>
2022-04-13 21:40:24 +0530

    removed comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,28 +2,10 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { useEffect, useState } from "react"
-//import { useState } from "react/cjs/react.production.min";
-//import contentfulPost from "../templates/contentfulPost"
-
 const IndexPage = ({ data }) => {
   const posts = data.allContentfulPost.edges
-  console.log(posts)
+  
   const [post, setPost] = useState([])
-
-  const renderData = () => {
-    console.log("hy")
-    console.log(posts)
-    post.map((item, index) => {
-      console.log("hy")
-      return (
-        <div>
-          <h5>{item.node.title}</h5>
-          <p>{item.node.subtitle}</p>
-          <p>{item.node.author}</p>
-        </div>
-      )
-    })
-  }
 
   useEffect(() => {
     setPost(posts)
@@ -33,7 +15,6 @@ const IndexPage = ({ data }) => {
     <Layout>
       <h3>welcome</h3>
       {post.map((item, index) => {
-        console.log("hy")
         return (
           <div>
             <h5>{item.node.title}</h5>
